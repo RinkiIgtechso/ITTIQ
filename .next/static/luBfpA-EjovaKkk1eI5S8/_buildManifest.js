db01fde1e0fb7a01aa6babea5ac30675f9106c33
RinkiIgtechso <kumaririnki@igtechso.com>
2023-06-13 11:06:33 +0530

    Updating .next folder

---
+++ .next/static/luBfpA-EjovaKkk1eI5S8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/353-2191ecd488929b17.js","static/css/66195c2e24f17aaf.css","static/chunks/pages/index-fa455dcf935dba14.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
